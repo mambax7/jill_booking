4aa56a5843477486263bda5e7c60c7e10b707a0d
mambax7 <mambax7@gmail.com>
2016-01-18 05:13:50 -0500

    Bring everything back up one level

---
+++ docs/readme.txt
@@ -0,0 +1,4 @@
+What is Jill Booking Module?
+======================
+
+The purpose of this module is to provide a Venue Booking module
---
+++ language/tchinese/modinfo.php
@@ -0,0 +1,25 @@
+<?php
+//  ------------------------------------------------------------------------ //
+// ���Ҳե� tnjaile �s�@
+// �s�@����G2015-01-23
+// $Id:$
+// ------------------------------------------------------------------------- //
+include_once XOOPS_ROOT_PATH . "/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+
+define('_MI_JILLBOOKIN_NAME', '���a�w��');
+define('_MI_JILLBOOKIN_AUTHOR', '���a�w��');
+define('_MI_JILLBOOKIN_CREDITS', '');
+define('_MI_JILLBOOKIN_DESC', '���Ҳժ��γ~�O���ѳ��a�w�����Ҳ�');
+define('_MI_JILLBOOKIN_AUTHOR_WEB', '');
+define('_MI_JILLBOOKIN_ADMENU1', "���a�޲z");
+define('_MI_JILLBOOKIN_ADMENU1_DESC', "���a�޲z");
+define("_MI_JILLBOOKIN_SMNAME2", "�妸�w��");
+define("_MI_JILLBOOKIN_SMNAME3", "�w���M��");
+//�϶�
+define('_MI_JB_B_TODAY_LIST_BLOCK_NAME', '������a�w�����p');
+define('_MI_JB_B_TODAY_LIST_BLOCK_DESC', '������a�w�����p�϶� (jb_b_today_list)');
+//���n�]�w
+define('_MI_JILLBOOKIN_BOOKING_GROUP', '�i�w�����a��');
+define('_MI_JILLBOOKIN_BOOKING_GROUP_DESC', '�i�w�����a���s��');
+define('_MI_JILLBOOKIN_CAN_CANCEL', '�i�����L�H�w�����a��');
+define('_MI_JILLBOOKIN_CAN_CANCEL_DESC', '�i�����L�H�w�����a���s��');
